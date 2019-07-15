a3693e1830a43b9579ac754ae428282928951862
J <janbielowka@gmail.com>
2019-07-15 14:34:55 +0200

    Like

--- Bot.py
+++ Bot.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 
@@ -18,6 +19,25 @@ class Bot:
         username_input.send_keys(self.username)
         pw_input.send_keys(self.password)
         pw_input.send_keys(Keys.RETURN)
+        time.sleep(2)
+
+    def like(self, hashtag, count):
+        driver = self.driver
+        driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
+        time.sleep(2)
+        first_pic = driver.find_element_by_tag_name("a")
+        first_pic.find_element_by_xpath("..").click()
+        i = 0
+        while i < count:
+            time.sleep(2)
+            try:
+                driver.find_element_by_xpath(
+                    "/html/body/div[3]/div[2]/div/article/div[2]/section[1]/span[1]/button").click()
+                time.sleep(18)
+            except NoSuchElementException:
+                time.sleep(2)
+            driver.find_element_by_link_text("Dalej").click()
+        self.close()
 
     def close(self):
         self.driver.close()
